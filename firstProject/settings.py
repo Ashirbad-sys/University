32fa3d3fc76aea45ef7feae8c9a51ce2dfbf75e2
Ashirbad-sys <ashirbadpattanayak05@gmail.com>
2025-08-06 23:06:11 +0530

    Fix: Added render domain to ALLOWED_HOSTS

--- firstProject/settings.py
+++ firstProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6s7eu2c-^ck$(!@9^mc6shn!d6w7@(lploh-os+o*utw!h*2w!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['university-fq13.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
