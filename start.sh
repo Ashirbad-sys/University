f7897cb59eaa5b83325287f3bf890438a54ddf18
Ashirbad-sys <ashirbadpattanayak05@gmail.com>
2025-08-13 21:21:40 +0530

    Update start.sh to use Gunicorn for production

--- start.sh
+++ start.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# Exit if any command fails
+set -o errexit  
+
+# Run migrations
 python manage.py migrate
+
+# Collect static files
 python manage.py collectstatic --noinput
+
+# Start Gunicorn server
 gunicorn firstProject.wsgi:application --bind 0.0.0.0:$PORT
